ab7f45fa9b69c8f5938fd472ed5def6be83beb63
Piotr Wasilewski <piotr.m.wasilewski@gmail.com>
2017-03-20 16:29:48 +0100

    bumped version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
     name='ansible-lint-junit',
     packages=['ansible-lint-junit'],
-    version='0.1',
+    version='0.2',
     description='ansible-lint to JUnit converter.',
     author='wasil',
     author_email='piotr.m.wasilewski@gmail.com',
