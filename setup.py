c77a081bdcf6e156a03b2069d74b0593768498f3
Piotr Wasilewski <piotr.m.wasilewski@gmail.com>
2017-03-21 00:01:39 +0100

    fixed executable path

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
     name='ansible-lint-junit',
     packages=['ansible-lint-junit'],
-    version='0.4',
+    version='0.5',
     description='ansible-lint to JUnit converter.',
     author='wasil',
     author_email='piotr.m.wasilewski@gmail.com',
     url='https://github.com/wasilak/ansible-lint-junit',
-    download_url='https://github.com/wasilak/ansible-lint-junit/archive/0.4.tar.gz',
+    download_url='https://github.com/wasilak/ansible-lint-junit/archive/0.5.tar.gz',
     keywords=['ansible', 'junit'],
     classifiers=[],
-    scripts=['ansible-lint-junit/ansible-lint-junit']
+    scripts=['bin/ansible-lint-junit']
 )
