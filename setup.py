5a3ca60f382795a19c4027027bb3aa58c4496e1f
Piotr Wasilewski <piotr.m.wasilewski@gmail.com>
2017-03-20 16:39:27 +0100

    renamed executable

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
     name='ansible-lint-junit',
     packages=['ansible-lint-junit'],
-    version='0.2',
+    version='0.3',
     description='ansible-lint to JUnit converter.',
     author='wasil',
     author_email='piotr.m.wasilewski@gmail.com',
     url='https://github.com/wasilak/ansible-lint-junit',
-    download_url='https://github.com/wasilak/ansible-lint-junit/archive/0.2.tar.gz',
+    download_url='https://github.com/wasilak/ansible-lint-junit/archive/0.3.tar.gz',
     keywords=['ansible', 'junit'],
     classifiers=[],
 )
